c57d239c28e22aa8959a7605baf46d6a3fd94a8a
Taraka Ramarao Naira <taraknaira@Tarakas-MacBook-Air.local>
2026-05-15 12:03:16 +0530

    updating favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,17 +31,20 @@ export const metadata: Metadata = {
   publisher: "Swastha Homoeopathy",
   icons: {
     icon: [
-      { url: "/icon.png", 
-        type: "image/png", 
-        sizes: "512x512" },
+      { url: "/favicon-16x16.png", 
+        sizes: "16x16", 
+        type: "image/png" },
+      { url: "/favicon-32x32.png", 
+        sizes: "32x32", 
+        type: "image/png" },
       { url: "/favicon.ico", 
         sizes: "any" }
     ],
-    apple: [
-      { url: "/apple-icon.png", 
-        sizes: "180x180", 
-        type: "image/png" }
-    ],
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png"
+    },
     shortcut: "/favicon.ico"
   },
   description:
